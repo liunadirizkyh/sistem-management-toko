4f09279454438aeceef81fb95ab11de5391e9d19
liu <liunadi.hidayat@binus.ac.id>
2025-09-11 10:43:10 +0700

    feat: add responsive navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,27 +1,21 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo height="h-12" width="w-12" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-    
                     @hasanyrole('admin|kasir')
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
-                
                         <x-nav-link :href="route('transaksi.index')" :active="request()->routeIs('transaksi.*')">
                             {{ __('Transaksi') }}
                         </x-nav-link>
-                
                         <x-nav-link :href="route('barang.index')" :active="request()->routeIs('barang.*')">
                             {{ __('Data Barang') }}
                         </x-nav-link>
@@ -31,7 +25,6 @@
                         <x-nav-link :href="route('kode-barang.index')" :active="request()->routeIs('kode-barang.*')">
                             {{ __('Kode Barang') }}
                         </x-nav-link>
-                
                         <x-nav-link :href="route('hutang-supplier.index')" :active="request()->routeIs('hutang-supplier.*')">
                             {{ __('Hutang Supplier') }}
                         </x-nav-link>
@@ -39,7 +32,6 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -59,7 +51,6 @@
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
@@ -73,7 +64,6 @@
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -85,15 +75,30 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            @hasanyrole('admin|kasir')
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('transaksi.index')" :active="request()->routeIs('transaksi.*')">
+                    {{ __('Transaksi') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('barang.index')" :active="request()->routeIs('barang.*')">
+                    {{ __('Data Barang') }}
+                </x-responsive-nav-link>
+            @endhasanyrole
+            
+            @role('admin')
+                <x-responsive-nav-link :href="route('kode-barang.index')" :active="request()->routeIs('kode-barang.*')">
+                    {{ __('Kode Barang') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('hutang-supplier.index')" :active="request()->routeIs('hutang-supplier.*')">
+                    {{ __('Hutang Supplier') }}
+                </x-responsive-nav-link>
+            @endrole
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
@@ -105,7 +110,6 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
